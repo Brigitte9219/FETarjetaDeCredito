0fac71a9547cace44bb5b85fd5fd25ecb62c4dd7
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-16 11:19:36 -0500

    Formulario reactivo - Funcionamiento del formulario (ReactiveFormsModule-FormGroup-FormBuilder-FormControl-FormControlName)

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -11,4 +12,31 @@ export class TarjetaCreditoComponent {
     { titular:'Juan Perez', numeroTarjeta:'123456',fechaExpiracion:'11/24', cvv:'123'},
     { titular:'Miguel Gonzalez', numeroTarjeta:'654321',fechaExpiracion:'12/24', cvv:'321'}
   ];
+
+  form: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.form = this.fb.group({
+      titular:[''],
+      numeroTarjeta:[''],
+      fechaExpiracion:[''],
+      cvv:['']
+    })
+  }
+
+  agregarTarjeta(){
+    console.log(this.form);
+
+    const tarjeta:any = {
+      titular: this.form.get('titular')?.value,
+        numeroTarjeta: this.form.get('titular')?.value,
+        fechaExpiracion: this.form.get('titular')?.value,
+        cvv: this.form.get('titular')?.value,
+    }
+
+    this.listTarjetas.push(tarjeta);
+    this.form.reset();
+  }
+
+
 }

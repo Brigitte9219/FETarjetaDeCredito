14768ec2d6f07f5811ddffa86c80eb027e4e16d2
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-19 07:44:56 -0500

    Se realiza el servicio para OBTENER EL LISTADO DE LAS TARJETAS (GET) y se crea el respectivo método en el componente

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,3 +1,4 @@
+import { TarjetaService } from './../../services/tarjeta.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
@@ -16,14 +17,26 @@ export class TarjetaCreditoComponent {
 
   form: FormGroup;
 
-  constructor(private fb:FormBuilder,
-    private toastr: ToastrService){
+  constructor(private fb:FormBuilder, private toastr: ToastrService,
+    private _tarjetaService:TarjetaService){
     this.form = this.fb.group({
       titular:['', Validators.required],
       numeroTarjeta:['',[Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
       fechaExpiracion:['', [Validators.required,Validators.minLength(5), Validators.maxLength(5)]],
       cvv:['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
     })
+  }
+
+  ngOnInit(): void {
+    this.obtenerTarjetas();
+  }
+
+  obtenerTarjetas(){
+    this._tarjetaService.getListTarjetas().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    } )
   }
 
   agregarTarjeta(){
--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarjetaService {
 
-  myAppUrl = 'https://localhost:7083/';
-  myApiUrl = 'api/tarjeta/'
+  private myAppUrl = 'https://localhost:7083/';
+  private myApiUrl = 'api/tarjeta/';
 
   constructor( private http:HttpClient) { }
+
+  getListTarjetas(): Observable<any>{
+    return this.http.get(this.myAppUrl+this.myApiUrl);
+  }
 }
